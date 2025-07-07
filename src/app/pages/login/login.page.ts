6a0fb74087e6ada8851575046e040de91ecbbc54
NathalyFer <na.fernandezr@duocuc.cl>
2025-07-06 20:12:36 -0400

    generación y firmado de APK

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,7 +2,7 @@ import { query } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-//import { MisDatosService } from 'src/app/services/mis-datos.service';
+import { MisDatosService } from 'src/app/services/mis-datos.service';
 
 
 
@@ -18,14 +18,16 @@ export class LoginPage {
   password: string = '';
   isDBReady: boolean = false
 
-  constructor( private router: Router,
-                private alertController: AlertController ) { }
-                //private misDatosService: MisDatosService//) { }
+  constructor(
+    private router: Router,
+    private alertController: AlertController,
+    private misDatosService: MisDatosService
+  ) { }
 
   ngOnInit() {
-   /* this.misDatosService.getIsDBReady().subscribe(isReady => {
+    this.misDatosService.getIsDBReady().subscribe(isReady => {
       this.isDBReady = isReady;
-    });*/
+    });
   }
     
   //metodo para mostrar alerta
@@ -42,47 +44,41 @@ export class LoginPage {
 
   //metodo login 
   async login() {
-      // Validaciones de campos
+    // Validaciones de campos
     if (!this.username || !this.password) {
       this.mostrarAlerta('Por favor, complete todos los campos.');
       return;
     }
 
     // Validar que el campo usuario tenga entre 3 y 8 caracteres
-      if (this.username.length < 3 || this.username.length > 8) {
-        this.mostrarAlerta('El usuario debe tener entre 3 y 8 caracteres.');
-        return;
-      }
+    if (this.username.length < 3 || this.username.length > 8) {
+      this.mostrarAlerta('El usuario debe tener entre 3 y 8 caracteres.');
+      return;
+    }
 
-      // Validar que la contraseña tenga 4 caracteres
-      if (this.password.length !== 4) {
-        this.mostrarAlerta('La contraseña debe tener 4 caracteres.');
-        return;
-      }
+    // Validar que la contraseña tenga 4 caracteres
+    if (this.password.length !== 4) {
+      this.mostrarAlerta('La contraseña debe tener 4 caracteres.');
+      return;
+    }
 
-      //si todas las validaciones son correctas, navegar a la página de home
-        this.router.navigate(['/home'], {
-          queryParams: { username: this.username, password: this.password }
-        });
-                          
-          }
-          
-       // Validar usuario en base de datos
-      /*const username = await this.misDatosService.validarUsuario(this.username, this.password);
+    // Validar usuario en base de datos
+    const usuarioValido: boolean = await this.misDatosService.validarUsuario(this.username, this.password);
 
-            // Valida para iniciar sesión
-      if (username) {
-        this.misDatosService.setUsername(this.username);
-        //si todas las validaciones son correctas
-        localStorage.setItem('usuarioActivo', 'true'); // simula sesión iniciada
-        
-        this.router.navigate(['/home'], {
-          queryParams: { username: this.username }
-        });
-      } else {
-        // Usuario inválido mostrar mensaje
-        this.mostrarAlerta('Usuario o contraseña incorrectos.');
-      }}*/
+    // Valida para iniciar sesión
+    if (usuarioValido) {
+      this.misDatosService.setUsername(this.username);
+      //si todas las validaciones son correctas
+      localStorage.setItem('usuarioActivo', 'true'); // simula sesión iniciada
+      
+      this.router.navigate(['/home'], {
+        queryParams: { username: this.username }
+      });
+    } else {
+      // Usuario inválido mostrar mensaje
+      this.mostrarAlerta('Usuario o contraseña incorrectos.');
+    }
+  }
     
 
   
